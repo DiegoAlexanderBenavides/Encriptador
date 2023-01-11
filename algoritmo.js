8d243e002400917a7f1abc2489d081445126905f
Diego <diegoven@unicauca.edu.co>
2023-01-10 21:43:49 -0500

    se realizo el algoritmo en javascript que permite encriptar un texto

--- algoritmo.js
+++ algoritmo.js
@@ -1,9 +1,31 @@
 var bt= document.getElementById('boton-encriptar')
+
+bt.addEventListener('click',fencriptar,false);
 function fencriptar(){
-   var texto= document.getElementById('texto-ingresado');
-   var textoh=texto.innerHTML;
-    console.log(textoh);
+   var texto= document.getElementById('texto-ingresado').value;
+   texto=texto.split('');
+   for (var i=0; i<texto.length;i++){
+    if (texto[i]=="a"){
+        texto[i]="ai";
+    }
+    if (texto[i]=="e"){
+        texto[i]="enter";
+    }
+    if (texto[i]=="i"){
+        texto[i]="imes";
+    }
+    if (texto[i]=="o"){
+        texto[i]="ober";
+    }
+    if (texto[i]=="u"){
+        texto[i]="ufat";
+    }
 
+   }
+    texto=texto.join('');
+    console.log(texto);
+   
 }
+bt.onclick="window.location.href=desktop2.html";
 
-bt.addEventListener('click',fencriptar,true);
+
